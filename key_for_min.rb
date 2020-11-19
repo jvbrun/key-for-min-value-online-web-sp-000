2020391e39c7261e4f60fce6633766c9e4096a0e
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-11-19 17:52:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  new_value = nil
-  key_for_min_value.each do |index, value|
-    if value < new_value 
-      new_value == value 
-    end
+  smallest_value = 0
+ empty_hash = nil
+ name_hash.each do |name, value|
+  if smallest_value == 0 || value < smallest_value
+    smallest_value = value
+    empty_hash = name
   end
-  index 
+ end
+ empty_hash
 end
